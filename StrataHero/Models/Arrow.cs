fd3252d7cdf665464173ce04d092e0242c9aa304
anek05 <andreas.eklund94@gmail.com>
2024-04-04 15:38:28 +0200

    Created Arrow class

---
+++ StrataHero/Models/Arrow.cs
@@ -0,0 +1,39 @@
+﻿namespace StrataHero.Models
+{
+    public class Arrow
+    {
+        public string ArrowSVG { get; set; }
+
+        private readonly Direction direction;
+        private readonly State state;
+
+
+        public Arrow(Direction direction, State state)
+        {
+            this.direction = direction;
+            this.state = state;
+        }
+
+        private void SetArrowSVG()
+        {
+            if(this.direction == Direction.Up)
+            {
+                ArrowSVG = "<svg class=\"up\" style=\"transform: rotate(270deg); color: @upColor;\" xmlns=\"http://www.w3.org/2000/svg\" width=\"19.99\" height=\"18\"><path fill=\"currentColor\" d=\"M10.029 5H0v7.967h10.029V18l9.961-9.048L10.029 0v5z\" /></svg>";
+            }
+            else if(this.direction == Direction.Down)
+            {
+                ArrowSVG = "<svg class=\"down\" style=\"transform: rotate(90deg); color: @downColor;\" xmlns=\"http://www.w3.org/2000/svg\" width=\"19.99\" height=\"18\"><path fill=\"currentColor\" d=\"M10.029 5H0v7.967h10.029V18l9.961-9.048L10.029 0v5z\" /></svg>";
+            }
+            else if (this.direction == Direction.Left)
+            {
+                ArrowSVG = "<svg class=\"left\" style=\"transform: rotate(180deg); color: @leftColor;\" xmlns=\"http://www.w3.org/2000/svg\" width=\"19.99\" height=\"18\"><path fill=\"currentColor\" d=\"M10.029 5H0v7.967h10.029V18l9.961-9.048L10.029 0v5z\" /></svg>";
+            }
+            else
+            {
+                ArrowSVG = "<svg class=\"right\" style=\"color: @rightColor;\" xmlns=\"http://www.w3.org/2000/svg\" width=\"19.99\" height=\"18\"><path fill=\"currentColor\" d=\"M10.029 5H0v7.967h10.029V18l9.961-9.048L10.029 0v5z\" /></svg>";
+            }
+        }
+    }
+    public enum Direction { Left, Right, Up, Down };
+    public enum State { Pressed, ToPress, Upcoming };
+}
